4467370fd0221a0e9a76e2c9e2aefefabc174ca8
Josep Conejero <bermeu@gmail.com>
2022-01-31 22:13:06 +0100

    se añaden datos de gentlemen

--- src/Components/Gentleman.js
+++ src/Components/Gentleman.js
@@ -1,26 +1,27 @@
-const Gentleman = ({ name }) => {
+const Gentleman = ({ datos }) => {
+  //const picture = {datos.picture};
   return (
     <li class="gentleman">
       <div class="gentleman__avatar-container">
         <img
           class="gentleman__avatar"
-          src="img/bertin.jpg"
+          src="bertin.jpg"
           alt="Bertin Osbourne pointing at you"
         />
         <span class="gentleman__initial">B</span>
       </div>
       <div class="gentleman__data-container">
-        <h2 class="gentleman__name">{name}</h2>
+        <h2 class="gentleman__name">{datos.name}</h2>
         <ul class="gentleman__data-list">
           <li class="gentleman__data">
             <span class="gentleman__data-label">Profession:</span>
-            Java developer
+            {datos.profession}
           </li>
           <li class="gentleman__data">
-            <span class="gentleman__data-label">Status:</span> Alive
+            <span class="gentleman__data-label">Status:</span> {datos.status}
           </li>
           <li class="gentleman__data">
-            <span class="gentleman__data-label">Twitter:</span> @osbourne
+            <span class="gentleman__data-label">Twitter:</span> {datos.twitter}
           </li>
         </ul>
       </div>
